490bcd0209c71b37dfd6442dc9573f58153b45d4
andreluizang <67346549+andreluizang@users.noreply.github.com>
2021-11-03 22:33:08 -0300

    Criando classes de teste

---
+++ SistemaA3/SistemaA3/test/ConteudoTestes.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ConteudoTestes {
+    
+    public ConteudoTestes() {
+    }
+
+    // @Test
+    // public void hello() {}
+}
---
+++ SistemaA3/SistemaA3/test/EstudanteTestes.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EstudanteTestes {
+    
+    public EstudanteTestes() {
+    }
+
+    // @Test
+    // public void hello() {}
+}
